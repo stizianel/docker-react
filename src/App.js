707219c31d20051be515d162ccc06aca92c464d7
stizianel <stizianel@gmail.com>
2020-08-01 18:32:31 +0200

    nuovo testo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Ciao Mona!
+          Edit <code>src/App.js</code> and save to reload. Ciao Mona Feature branch!
         </p>
         <a
           className="App-link"
